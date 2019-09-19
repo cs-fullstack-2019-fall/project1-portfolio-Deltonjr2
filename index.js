f939b441aea2143a9904bca26e4129746dedcb1a
Deltonjr2 <Demetria.Farewell@yahoo.com>
2019-09-19 14:27:06 -0500

    profile cont'd

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
- return $.fn.button to previously assigned value
-          give $().bootstrapBtn the Bootstrap functionality
+ // return $.fn.button to previously assigned value
+ //          give $().bootstrapBtn the Bootstrap functionality
+
+ function myFunction() {
+     var x = document.getElementById("navDemo");
+     if (x.className.indexOf("w3-show") == -1) {
+         x.className += " w3-show";
+     } else {
+         x.className = x.className.replace(" w3-show", "");
+     }
+ }
+
+
+ var modal = document.getElementById('ticketModal');
+ window.onclick = function(event) {
+     if (event.target == modal) {
+         modal.style.display = "none";
+     }
+ }
