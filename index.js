92e5e207238e416c26b7e27470b82d8f3b7bdcf7
Deltonjr2 <Demetria.Farewell@yahoo.com>
2019-09-20 07:59:00 -0500

    project

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
- // return $.fn.button to previously assigned value
+ return $.fn.button to previously assigned value
  //          give $().bootstrapBtn the Bootstrap functionality
-
- function myFunction() {
-     var x = document.getElementById("navDemo");
-     if (x.className.indexOf("w3-show") == -1) {
-         x.className += " w3-show";
-     } else {
-         x.className = x.className.replace(" w3-show", "");
-     }
+ //
+ // function myFunction() {
+ //     var x = document.getElementById("navDemo");
+ //     if (x.className.indexOf("w3-show") == -1) {
+ //         x.className += " w3-show";
+ //     } else {
+ //         x.className = x.className.replace(" w3-show", "");
+ //     }
+ // }
+ //
+ //
+ // var modal = document.getElementById('ticketModal');
+ // window.onclick = function(event) {
+ //     if (event.target == modal) {
+ //         modal.style.display = "none";
+ //     }
  }
-
-
- var modal = document.getElementById('ticketModal');
- window.onclick = function(event) {
-     if (event.target == modal) {
-         modal.style.display = "none";
-     }
- }
+ // document.body.classList.add('js-loading');
+ //
+ // window.addEventListener("load", showPage);
+ //
+ // function showPage() {
+ //     document.body.classList.remove('js-loading');
+ // }
+ // var img = document.querySelector("img");
+ // document.body.classList.add('js-loading');
+ // img.addEventListener("load", removeLoadingClass);
+ // function removeLoadingClass() {
+ //     document.body.classList.remove('js-loading');
+ // }
